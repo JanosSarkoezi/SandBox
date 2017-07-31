92988812d766729c85fd210702be27ff41f159d1
JanosSarkoezi <j.sarkoezi@googlemail.com>
2017-07-31 18:53:34 +0200

    template für develop

---
+++ src/main/java/de/saj/sandbox/console/Main.java
@@ -0,0 +1,10 @@
+package de.saj.sandbox.console;
+
+/**
+ * @author saj
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        new Processor().process();
+    }
+}
---
+++ src/main/java/de/saj/sandbox/console/Processor.java
@@ -0,0 +1,9 @@
+package de.saj.sandbox.console;
+
+/**
+ * @author saj
+ */
+public class Processor {
+    public void process() {
+    }
+}
